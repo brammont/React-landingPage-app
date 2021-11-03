3ae3b48c900bea9236f19f23888aa6f8771a43d6
Brammont <brayanmazuera1@gmail.com>
2021-11-03 14:32:18 -0500

    agregando react-icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {MdFingerprint} from 'react-icons/md'
+import {Fabars,FaTimes} from 'react-icons/fa'
 function Navbar() {
     return (
         <>
         <div className="navbar">
             <div className="navbar-container container">
                 <Link to="/" className="navbar-logo">
-                    LAVAISH
+                    <MdFingerprint
+                     className="nav-icon"
+                    LAVAISH/>
                 </Link>
                 <div className="menu-icon">
-                    {}
+                    
                 </div>
             </div>
         </div>  
