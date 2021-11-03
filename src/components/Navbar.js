8467168460d739c28553da3687127ab79be89ebe
Brammont <brayanmazuera1@gmail.com>
2021-11-03 14:18:17 -0500

    instalar react-dom-router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 function Navbar() {
     return (
         <>
         <div className="navbar">
             <div className="navbar-container container">
-                
+                <Link to="/" className="navbar-logo">
+                    LAVAISH
+                </Link>
+                <div className="menu-icon">
+                    {}
+                </div>
             </div>
         </div>  
         </>
