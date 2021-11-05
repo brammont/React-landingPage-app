02110edf5f92967fb2547e44413c0727de0e97ac
Brammont <brayanmazuera1@gmail.com>
2021-11-05 14:06:34 -0500

    creando la pagina services

---
+++ src/components/pages/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Services.css'
+function Services() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+import './Services.css'
+export default Services
