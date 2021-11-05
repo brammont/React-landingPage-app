e1f6c5a9b95352fd4ab1073a980437b003ac8cfc
Brammont <brayanmazuera1@gmail.com>
2021-11-05 14:23:38 -0500

    estructurando pagina sing up

--- src/components/pages/SingUp/SignUp.js
+++ src/components/pages/SingUp/SignUp.js
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
 function SignUp() {
     return (
         <div>
-            
+            <HeroSection {...homeObjOne} />
+            <HeroSection {...homeObjThree} />
         </div>
     )
 }
