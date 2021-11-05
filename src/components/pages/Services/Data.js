ecac268934c5964f6afa022750e665c9104c718c
Brammont <brayanmazuera1@gmail.com>
2021-11-05 14:12:39 -0500

    creando datos para setup de compoenente en services

---
+++ src/components/pages/Services/Data.js
@@ -0,0 +1,28 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Acceso exclusivo',
+    headline: 'Transacciones ilimitadas con cero comisiones',
+    description:
+      'Obtenga acceso a nuestra exclusiva tarjeta de diamantes que le permite enviar transacciones ilimitadas sin que se le cobre ninguna tarifa.',
+    buttonLabel: 'Empezar',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Tarjeta de crédito'
+  };
+  
+  export const homeObjTwo = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'CONFIGURACIÓN FÁCIL',
+    headline: 'Proceso de incorporación superrápido y sencillo',
+    description:
+      "Tenga todo configurado y listo en menos de 10 minutos. Todo lo que necesita hacer es agregar su información y estará listo para comenzar.",
+    buttonLabel: 'Empezar ahora',
+    imgStart: 'empezar',
+    img: 'images/svg-7.svg',
+    alt: 'Bóveda'
+  };
+  
