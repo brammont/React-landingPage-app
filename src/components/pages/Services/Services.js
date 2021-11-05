1d4e6633b2916955ac0555545406792601aa6774
Brammont <brayanmazuera1@gmail.com>
2021-11-05 14:17:56 -0500

    estrcuturando la pagina services con sus componentes

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
 import Pricing from '../../Pricing';
 
 function Services() {
     return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default Services
+      <>
+        <Pricing />
+        <HeroSection {...homeObjOne} />
+        <HeroSection {...homeObjThree} />
+      </>
+    );
+  }
+  
+  export default Services;
