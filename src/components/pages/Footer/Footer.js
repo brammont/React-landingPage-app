130e5d04abf6645d8ea9a8a0d351d038caa5d59b
Brammont <brayanmazuera1@gmail.com>
2021-11-05 11:53:31 -0500

    importanto iconos para el compoente footer

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -1,5 +1,15 @@
 import React from 'react'
-
+import './Footer.css';
+import { Button } from '../../Button';
+import { Link } from 'react-router-dom';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import { MdFingerprint } from 'react-icons/md';
 function Footer() {
     return (
         <div>
